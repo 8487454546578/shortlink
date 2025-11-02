c7dc735f9dfc0fdaef774857e12f2e970601dc7d
duoge <1518199740@qq.com>
2025-11-02 15:39:38 +0800

    updated

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * 用户管理控制层
+ * */
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    /**
+     * 根据用户名查询信息
+     */
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public String getUserByUserName(@PathVariable("username") String username) {
+        return "Hi " + username;
+    }
+
+}
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/dto/resp/UserRespDTO.java
@@ -0,0 +1,10 @@
+package com.nageoffer.shortlink.admin.dto.resp;
+
+import lombok.Data;
+
+/**
+ * 用户返回实体
+ */
+@Data
+public class UserRespDTO {
+}
